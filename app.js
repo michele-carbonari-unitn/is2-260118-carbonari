7ffc5378e80dd439d984f7b4294c1d7f7bff908a
michele <michelecarbon@gmail.com>
2018-01-26 11:36:16 +0100

    heroku service created

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 const express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    fetch = require('node-fetch')
+    checker=require('./checker.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,8 +11,11 @@ app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
 // a useless function that returns a fixed object. you can use it, if you want, for testing purposes
-app.get('/count',function (req, res) {
-    res.json({count: 5})
+app.post('/count',function (req, res) {
+    parametri=req.body.json;
+    risposta=checker.check(parametri.url, parametri.invocationParameter, parametri.expectedResultData, parametri.expectedResultStatus);
+    var risposta1=JSON.stringify(risposta);
+    res.send(risposta1);
 })
 
 app.listen(app.get('port'), function() {
